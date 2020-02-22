7d2f69b00e28d914436b3bdcc0482423288792b0
Michael Eliot <michael.eliot@berkeley.edu>
2020-02-21 23:25:36 -0800

    Updates

---
+++ precache-manifest.3d6ff8bc395b27ce25683ab27f3b4b15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "658b7f87d5064122364a5b56424209dc",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "509cd93bc3f9ed4311b1",
+    "url": "/trivia/static/css/main.e65619c7.chunk.css"
+  },
+  {
+    "revision": "030fabe9dc0e1b3a3780",
+    "url": "/trivia/static/js/2.5d4f41d1.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/trivia/static/js/2.5d4f41d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "509cd93bc3f9ed4311b1",
+    "url": "/trivia/static/js/main.a27c94d7.chunk.js"
+  },
+  {
+    "revision": "4a0429a3bd8d90900b62",
+    "url": "/trivia/static/js/runtime-main.a7052699.js"
+  }
+]);
